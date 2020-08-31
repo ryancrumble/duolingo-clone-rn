cca1ce448a3418ed5ef593a50307ed44b6827189
Ryan Crummy <ryan.crummy@twobulls.com>
2020-08-31 12:24:36 +1000

    added low res icons to ShopScreen
    added low res icons from Icons8

---
+++ assets/icons/attribute.md
@@ -0,0 +1,3 @@
+# Attribute
+
+- Icons made by [Icons8](https://icons8.com/) from [www.icons8.com](https://icons8.com)
--- src/components/Shop/ShopCard.tsx
+++ src/components/Shop/ShopCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { colors } from '../../styles/globalStyles';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { ShopItem } from '../../types/data';
@@ -8,15 +8,46 @@ interface Props {
   item: ShopItem;
 }
 
+enum ShopIcon {
+  ICEBERG = 'ICEBERG',
+  FIRE = 'FIRE',
+  SUIT = 'SUIT',
+  TRACKSUIT = 'TRACKSUIT',
+  SUPERHERO = 'SUPERHERO',
+  CONVERSATION = 'CONVERSATION',
+  DATING = 'DATING',
+}
+
 const containerHeight = 160;
 
 const ShopCard: React.FC<Props> = ({ item }) => {
   const { name, description, tokenCost, image } = item;
 
+  const shopIcon = () => {
+    switch (image) {
+      case ShopIcon.ICEBERG:
+        return require('../../../assets/icons/iceberg.png');
+      case ShopIcon.FIRE:
+        return require('../../../assets/icons/fire.png');
+      case ShopIcon.SUIT:
+        return require('../../../assets/icons/suit.png');
+      case ShopIcon.TRACKSUIT:
+        return require('../../../assets/icons/tracksuit.png');
+      case ShopIcon.SUPERHERO:
+        return require('../../../assets/icons/superhero.png');
+      case ShopIcon.CONVERSATION:
+        return require('../../../assets/icons/chatroom-outline.png');
+      case ShopIcon.DATING:
+        return require('../../../assets/icons/heart-outline.png');
+      default:
+        return require('../../../assets/icons/fire.png');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.sectionIcon}>
-        <Text>Icon</Text>
+        <Image style={{ width: 100, height: 100 }} resizeMode="contain" source={shopIcon()} />
       </View>
       <View style={styles.sectionDetails}>
         <Text style={styles.titleText}>{name}</Text>
@@ -48,6 +79,7 @@ const styles = StyleSheet.create({
     // borderColor: 'lightgreen',
     // borderWidth: 2,
     width: 140,
+    paddingTop: 10,
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
